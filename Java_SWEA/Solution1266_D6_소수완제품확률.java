21fb978074b76ad59d82b45b3577394c1e9d9c17
강민 <ultimate931218@gmail.comcd>
2019-02-12 18:45:32 +0900

    last upload

--- Java_SWEA/Solution1266_D6_소수완제품확률.java
+++ Java_SWEA/Solution1266_D6_소수완제품확률.java
@@ -9,13 +9,93 @@ public class Solution1266_D6_소수완제품확률 {
 	 * Hint: nCr * p^r * (1-p)^(n-r)
 	 * */
 	public static int[] combifact;
+	public static double[] a = new double[19];
+	public static double[] b = new double[19];
+	public static String[] line;
+	public static double pa, pb, ans;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = Integer.parseInt(br.readLine().trim());
+		setCombiFact();
+		for(int t=1;t<=tc;t++) {
+			line = br.readLine().split(" ");
+			pa = (double) Integer.parseInt(line[0]) / 100.0;
+			for(int i=0;i<=18;i++) {
+				a[i] = combifact[i] * power(pa,i) * power(1-pa,18-i);
+			}
+			pb = (double) Integer.parseInt(line[1]) / 100.0;
+			for(int i=0;i<=18;i++) {
+				b[i] = combifact[i] * power(pb,i) * power(1-pb,18-i);
+			}
+			ans = notPrimeSelectA() * notPrimeSelectB();
+			System.out.printf("#%d %.6f\n",t,1-ans);
+		}
 	}
-
+	public static double notPrimeSelectA() {
+		double ret = 0;
+		for(int i=0;i<=18;i++) {
+			if(!(i==2||i==3||i==5||i==7||i==11||i==13||i==17)) {
+				ret += a[i];
+			}
+		}
+		return ret;
+	}
+	public static double notPrimeSelectB() {
+		double ret = 0;
+		for(int i=0;i<=18;i++) {
+			if(!(i==2||i==3||i==5||i==7||i==11||i==13||i==17)) {
+				ret += b[i];
+			}
+		}
+		return ret;
+	}
+	public static double power(double d, int n) {
+		double ret = 1;
+		for(int i=0;i<n;i++) {
+			ret *= d;
+		}
+		return ret;
+	}
+	public static void setCombiFact() {
+		combifact = new int[19];
+		combifact[0] = combifact[18] = 1;
+		int top = 18; int bot = 1; 
+		int temp = top / bot;
+		for(int i=1;i<=9;i++) {
+			combifact[i] = temp;
+			combifact[18-i] = temp;
+			top--; bot++;
+			temp = temp * top / bot;
+		}
+	}
 }
+/*
+10
+0 0
+80 90
+10 20
+1 2
+100 100
+60 23
+70 23
+80 32
+90 32
+100 32
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import java.io.BufferedReader;
@@ -87,4 +167,3 @@ public class Solution {
  
 }
  */
-*/
