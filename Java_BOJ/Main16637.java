103f873edf2f79af5e3725e8aeb2edc96056b189
zzangkkmin <zzangkkmin@naver.com>
2019-10-16 08:52:08 +0900

    ADD 밀린커밋

---
+++ Java_BOJ/Main16637.java
@@ -0,0 +1,91 @@
+package com.ssafy.git.Java_BOJ;
+
+import java.util.Scanner;
+
+public class Main16637 {
+	static int n, arIdx, numIdx;
+	static int ans = -2147483648;
+	static String input;
+	static char[] arithmetic;
+	static int[] numbers;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		arithmetic = new char[n/2 + 1];
+		numbers = new int[n/2+1];
+		input = sc.next();
+		for(int i=0;i<input.length();i++) {
+			if(i%2==0) {
+				numbers[numIdx++] = input.charAt(i) - '0';
+			}else {
+				arithmetic[arIdx++] = input.charAt(i);
+			}
+		}
+		if(n==1) {
+			ans = numbers[0];
+		}else if(n==3) {
+			ans = bracketCal(arithmetic[0], 0);
+		} else {
+			solve(0,'?',0);
+		}
+		System.out.println(ans);
+		sc.close();
+	}
+	public static void solve(int x, char mode, int idx) {
+		if(mode == '?') {
+			solve(numbers[idx],arithmetic[idx],idx+1);
+			solve(bracketCal(arithmetic[idx], idx), arithmetic[idx+1], idx+2);
+		}
+		else if(idx == numIdx - 1) {
+			int ret = 0;
+			if(mode == '+') {
+				ret = x + numbers[idx];				
+			} else if(mode == '-') {
+				ret = x - numbers[idx];
+			} else if(mode == '*') {
+				ret = x * numbers[idx];				
+			} else if(mode == '/') {
+				ret = x / numbers[idx];			
+			}
+			ans = ans < ret ? ret : ans;
+			return;
+		}
+		else if(idx == numIdx) {
+			int ret = x;
+			ans = ans < ret ? ret : ans;
+			return;
+		}
+		else if(idx > numIdx) {
+			return;
+		}
+		else {
+			if(mode == '+') {
+				solve(x + numbers[idx], arithmetic[idx], idx + 1);
+				solve(x + bracketCal(arithmetic[idx], idx), arithmetic[idx+1], idx+2);
+			} else if(mode == '-') {
+				solve(x - numbers[idx], arithmetic[idx], idx + 1);
+				solve(x - bracketCal(arithmetic[idx], idx), arithmetic[idx+1], idx+2);
+			} else if(mode == '*') {
+				solve(x * numbers[idx], arithmetic[idx], idx + 1);
+				solve(x * bracketCal(arithmetic[idx], idx), arithmetic[idx+1], idx+2);
+			} else if(mode == '/') {
+				solve(x / numbers[idx], arithmetic[idx], idx + 1);
+				solve(x / bracketCal(arithmetic[idx], idx), arithmetic[idx+1], idx+2);
+			}
+		}
+	}
+	public static int bracketCal(char mode, int idx) {
+		int ret = 0;
+		if(mode == '+') {
+			ret = numbers[idx] + numbers[idx+1];
+		} else if(mode == '-') {
+			ret = numbers[idx] - numbers[idx+1];
+		} else if(mode == '*') {
+			ret = numbers[idx] * numbers[idx+1];
+		} else if(mode == '/') {
+			ret = numbers[idx] / numbers[idx+1];
+		}
+		return ret;
+	}
+}
--- Java_BOJ/Main17497.java
+++ Java_BOJ/Main17497.java
@@ -1,7 +1,5 @@
 package com.ssafy.git.Java_BOJ;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main17497 {
@@ -10,21 +8,16 @@ public class Main17497 {
 	static int ansCnt = -1;
 	static String ansStr = "";
 	static boolean findAns;
-	static HashSet<Long> visit = new HashSet<Long>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		setLimit();
 		Scanner sc = new Scanner(System.in);
 		x = sc.nextLong();
-		calculate("", 0, 0);
-		if(ansCnt == -1) {
-			System.out.println(ansCnt);
-		} else {
-			System.out.println(ansCnt);
-			for(int i=0;i<ansStr.length();i++) {
-				System.out.print("["+ansStr.charAt(i)+"] ");
-			}
-			System.out.println();
+		calculate();
+		ansCnt = ansStr.length();
+		System.out.println(ansCnt);
+		for(int i=ansCnt-1;i>=0;i--) {
+			System.out.print("["+ansStr.charAt(i)+"] ");
 		}
 		sc.close();
 	}
@@ -34,27 +27,17 @@ public class Main17497 {
 		}
 		upperLimit -= 1;
 	}
-	public static void calculate(String steps, int cnt, long target) {
-		//System.out.println(steps);
-		if(target == x) {
-			findAns = true;
-			ansCnt = cnt;
-			ansStr = steps;
-			return;
-		}
-		if(findAns || cnt>99 ||
-		   target < 0 || target > upperLimit ||
-		   visit.contains(target)) {
-			return;
-		} else {
-			visit.add(target);
-			if(target > x) {
-				calculate(steps.concat("/"), cnt+1, target/2);
-				calculate(steps.concat("-"), cnt+1, target-2);
-			} else{
-				calculate(steps.concat("*"), cnt+1, target*2);
-				calculate(steps.concat("+"), cnt+1, target+2);
-			
+	public static void calculate() {
+		while(x>0) {
+			if((x&1) != 0) {
+				ansStr = ansStr.concat("/");
+				x *= 2;
+			} else if((x&2) != 0) {
+				ansStr = ansStr.concat("+");
+				x -= 2;
+			} else {
+				ansStr = ansStr.concat("*");
+				x /= 2;
 			}
 		}
 	}
